2fd40e28b45adf047f54a575e1f83b9f22f3e384
Marlon Jaimes <andres_jr19@hotmail.com>
2022-10-23 18:04:12 -0500

    funciones basicas

---
+++ 07_funciones.py
@@ -0,0 +1,23 @@
+print("hola papu")
+
+
+def my_print(text):
+    print(text * 2)
+
+
+my_print("ah caray")
+my_print("Hola")
+
+
+def suma(a, b):
+    print(a+b)
+
+
+suma(20, 50)
+
+
+def sumaDoble(a, b):
+    my_print(a+b)
+
+
+sumaDoble(20, 50)
